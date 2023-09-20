ea52d822d3aadbd60b15131b25780c420502101d
Sten Matten Loo <poisonpeer@gmail.com>
2023-09-20 14:48:10 +0300

    something

--- script.js
+++ script.js
@@ -1,66 +1,37 @@
-let num = 2;
+function hello(){
+    console.log('hello1');
+}
+let hello2 = function (){
+    console.log('hello2');
+}
 
-console.log(num);
+let hello3 = () => {
+    console.log('hello13');
+}
 
-num = num + 2;
+let hello4 = () => console.log('hello4');
 
-console.log(num);
+hello();
+hello2();
+hello3();
+hello4();
 
-num = 5 + 2 * 4 / 6 - 6;
+let person = {
+    name: 'Kaspar',
+    age: '30',
+    describe(){
+        console.log('name' + this.name + ' age:' + this.age);
+    },
+    describe3: function() {
+        console.log(this);
+        console.log('name:' + this.name + ' age:' + this.age);
 
-num += 3;
-num -= 3;
-num *= 4;
-num /= 4;
-num++;
-num--;
-num = 13 % 2;
-num = 2;
-num = num**3;
-console.log(typeof num);
-let string = 'hello world';
-string += '!!!!!';
-console.log(string);
-console.log(typeof string);
-let bool = true;
-bool = false;
+    }
+}
 
-bool = false && true;
-bool = false || true;
-bool = !bool;
-bool = ture;
-num = 10;
-bool = 2 < num;
-bool = 2 > num;
-bool = 10 < num;
-bool = 10 == num;
-bool = 10 <= num;
-bool = 10 >= num;
-bool = 10 != num;
-bool = '10' === num;
-bool = '10' !== num;
+    person.name = 'Martin';
+    person.age = 33;
 
-console.log(bool);
-console.log(typeof bool);
-
-let novalue = null;
-let novalue2;
-
-console.log(novalue2); //undefined
-num = 0 * -1;
-num = 0.1 + 0.2;
-console.log(num);
-let array = [1, 2, 3, 4];
-array = [1, hello, true, undefined, null];
-console.log(array);
-console.log(array[0]);
-array[0] = 7;
-console.log(array);
-array = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
-console.log(array);
-console.log(array[1][2]);
-array = [   ]
+    person.describe();
+    person.describe2();
+    person.describe3();
